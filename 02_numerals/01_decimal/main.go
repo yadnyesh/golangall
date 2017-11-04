67e0bafdeaf9f6213c43c604994d85a8215890b3
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-11-04 14:50:50 +0530

    Normal Function

--- 02_numerals/01_decimal/main.go
+++ 02_numerals/01_decimal/main.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 )
 
+var x = 0
+
 func main() {
 
-	x := 0
-	increment := func() int {
-		x++
-		return x
-	}
+	//x := 0
+	//increment := func() int {
+	//	x++
+	//	return x
+	//}
 
 	fmt.Println(increment())
 	fmt.Println(increment())
 }
 
+func increment() int {
+	x++
+	return x
+}
