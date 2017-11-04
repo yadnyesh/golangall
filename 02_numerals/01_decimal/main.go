4f5a1465207706a73b99fbb5a50cd01072efd403
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-11-04 10:39:21 +0530

    Print Decimal, Binary and Hexadecimal

---
+++ 02_numerals/01_decimal/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//print decimal
+	fmt.Println(42)
+
+	//print binary
+	fmt.Printf("%d - %b\n", 42, 42)
+
+	//print Hexadecimal
+	fmt.Printf("%d - %#x\n", 42, 42)
+}
+
