f0295fdda4521e5b2319aacda83b4eed40f55c5c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-11-05 08:40:39 +0530

    Printing non-Strings

--- 02_numerals/01_decimal/main.go
+++ 02_numerals/01_decimal/main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"io/ioutil"
+	"strconv"
 )
 
 
 func main() {
 
-	res, _ := http.Get("https://www.google.co.in")
-
-	page, _ := ioutil.ReadAll(res.Body)
-
-	res.Body.Close()
-
-	fmt.Printf("%s", page)
+	const q  = "ABC"
+	const (
+		Pi = 3.14
+		Language = "Go"
+	)
+	//res, _ := http.Get("https://www.google.co.in")
+	//
+	//page, _ := ioutil.ReadAll(res.Body)
+	//
+	//res.Body.Close()
+	//
+	//fmt.Printf("%s", page)
+	fmt.Printf(strconv.FormatFloat(Pi, 'g', 1, 64))
+	fmt.Printf(Language)
+	fmt.Printf(q)
 
 }
 
