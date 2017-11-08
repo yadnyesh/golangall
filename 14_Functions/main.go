a70e3ed54ab86dc87bf1a90d1389fce900cb896b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-11-08 17:43:58 +0530

    Return from Function

--- 14_Functions/main.go
+++ 14_Functions/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	greet ("Jane", "Done")
+	//s := greet ("Jane", "Done")
+	fmt.Println(greet ("Jane", "Done"))
 }
 
-func greet(fname string, lname string) {
-	fmt.Println(fname , lname)
+func greet(fname string, lname string) string {
+	return fmt.Sprint(fname, lname)
 }
