69f568a703c7bf4f9c2f1ebaf9df7aade33186f6
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-11-08 17:46:53 +0530

    Multiple returns from Function

--- 14_Functions/main.go
+++ 14_Functions/main.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	//s := greet ("Jane", "Done")
 	fmt.Println(greet ("Jane", "Done"))
 }
 
-func greet(fname string, lname string) string {
-	return fmt.Sprint(fname, lname)
+func greet(fname, lname string) (string, string) {
+	return fmt.Sprint(fname, lname), fmt.Sprint(fname, lname)
 }
